5525d5647439aaf608c398a629feef623d2e0340
fueledbyflux <fueledbyflux@gmail.com>
2015-09-18 22:18:49 +1000

    Teemo

---
+++ TeemoBuddy/TeemoBuddy/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+
+namespace TeemoBuddy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+
+        public static Spell.Targeted Q;
+        public static Spell.Active W;
+        public static Spell.Active E;
+        public static Spell.Skillshot R;
+        public static Menu TeemoMenu, ComboMenu, HarassMenu, FarmMenu, FleeMenu;
+
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            TargetSelector2.init();
+            Bootstrap.Init(null);
+
+            Q = new Spell.Targeted(SpellSlot.Q, 680);
+            W = new Spell.Active(SpellSlot.W);
+            E = new Spell.Active(SpellSlot.E);
+            R = new Spell.Skillshot(SpellSlot.R, 300, SkillShotType.Circular, 500, 1000, 120);
+
+            TeemoMenu = MainMenu.AddMenu("TeemoBuddy", "teemobuddy");
+            TeemoMenu.AddGroupLabel("TeemoBuddy");
+            TeemoMenu.AddSeparator();
+            TeemoMenu.AddLabel("Always Remember, Buck Frandon.");
+            TeemoMenu.AddLabel("Made By Fluxy");
+
+            ComboMenu = TeemoMenu.AddSubMenu("Combo", "Combo");
+            ComboMenu.AddGroupLabel("Combo Settings");
+            ComboMenu.AddSeparator();
+            ComboMenu.Add("useQCombo", new CheckBox("Use Q"));
+            ComboMenu.Add("useWCombo", new CheckBox("Use W"));
+            ComboMenu.Add("useRCombo", new CheckBox("Use R"));
+
+            HarassMenu = TeemoMenu.AddSubMenu("Harass", "Harass");
+            HarassMenu.AddGroupLabel("Harass Settings");
+            HarassMenu.AddSeparator();
+            HarassMenu.Add("useQHarass", new CheckBox("Use Q"));
+            HarassMenu.Add("useWHarass", new CheckBox("Use W"));
+
+            FarmMenu = TeemoMenu.AddSubMenu("Farm", "Farm");
+            FarmMenu.AddGroupLabel("Farming Settings");
+            FarmMenu.AddSeparator();
+            FarmMenu.Add("useQFarmLH", new CheckBox("Use Q for LastHit"));
+            FarmMenu.Add("useQFarmWC", new CheckBox("Use Q for WaveClear"));
+
+            FleeMenu = TeemoMenu.AddSubMenu("Flee", "Flee");
+            FleeMenu.AddGroupLabel("Flee Settings");
+            FleeMenu.AddSeparator();
+            FleeMenu.Add("useRFlee", new CheckBox("Use R"));
+            FleeMenu.Add("useWFlee", new CheckBox("Use W"));
+
+            Game.OnTick += Game_OnTick;
+        }
+
+        private static void Game_OnTick(EventArgs args)
+        {
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo))
+            {
+                StateHandler.Combo();
+            }
+            else if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass))
+            {
+                StateHandler.Harass();
+            }
+            else if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LaneClear))
+            {
+                StateHandler.WaveClear();
+            }
+            else if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LastHit))
+            {
+                StateHandler.LastHit();
+            }
+            else if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Flee))
+            {
+                StateHandler.Flee();
+            }
+        }
+    }
+}
---
+++ TeemoBuddy/TeemoBuddy/StateHandler.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu.Values;
+
+namespace TeemoBuddy
+{
+    class StateHandler
+    {
+        public static AIHeroClient _Player { get { return ObjectManager.Player; } }
+        public static float GetDynamicRange()
+        {
+            if (Program.Q.IsReady())
+            {
+                return Program.Q.Range;
+            }
+            return _Player.GetAutoAttackRange();
+        }
+        public static void Combo()
+        {
+            var target = TargetSelector2.GetTarget(GetDynamicRange() + 100, DamageType.Magical);
+            if (target == null) return;
+
+            if (Program.ComboMenu["useQCombo"].Cast<CheckBox>().CurrentValue && Program.Q.IsReady() && target.IsValidTarget(Program.Q.Range))
+            {
+                Program.Q.Cast(target);
+            }
+            else if(Program.ComboMenu["useWCombo"].Cast<CheckBox>().CurrentValue && Program.W.IsReady() && target.Distance(_Player) > _Player.GetAutoAttackRange(target))
+            {
+                Program.W.Cast();
+            }
+            else if (Program.ComboMenu["useRCombo"].Cast<CheckBox>().CurrentValue && Program.R.IsReady() && target.IsValidTarget(Program.R.Range))
+            {
+                Program.R.Cast(target);
+            }
+        }
+
+        public static void Harass()
+        {
+            var target = TargetSelector2.GetTarget(GetDynamicRange() + 100, DamageType.Magical);
+            if (target == null) return;
+
+            if (Program.HarassMenu["useQHarass"].Cast<CheckBox>().CurrentValue && Program.Q.IsReady() && target.IsValidTarget(Program.Q.Range))
+            {
+                Program.Q.Cast(target);
+            }
+            else if (Program.HarassMenu["useWHarass"].Cast<CheckBox>().CurrentValue && Program.W.IsReady() && target.Distance(_Player) > _Player.GetAutoAttackRange(target))
+            {
+                Program.W.Cast();
+            }
+        }
+
+        public static void LastHit()
+        {
+            if (Program.FarmMenu["useQFarmLH"].Cast<CheckBox>().CurrentValue && Program.Q.IsReady())
+            {
+                var minion = ObjectManager.Get<Obj_AI_Minion>().FirstOrDefault(a => a.IsEnemy && a.Health <= QDamage(a));
+                if (minion == null) return;
+                Program.Q.Cast(minion);
+            }
+        }
+
+        public static void WaveClear()
+        {
+            if (Program.FarmMenu["useQFarmWC"].Cast<CheckBox>().CurrentValue && Program.Q.IsReady())
+            {
+                var minion = ObjectManager.Get<Obj_AI_Minion>().FirstOrDefault(a => a.IsEnemy && a.Health <= QDamage(a));
+                if (minion == null) return;
+                Program.Q.Cast(minion);
+            }
+        }
+
+        public static void Flee()
+        {
+            if (Program.FleeMenu["useRFlee"].Cast<CheckBox>().CurrentValue && Program.R.IsReady())
+            {
+                Program.R.Cast(_Player.Position);
+            }
+            if (Program.FleeMenu["useWFlee"].Cast<CheckBox>().CurrentValue && Program.W.IsReady())
+            {
+                Program.W.Cast();
+            }
+        }
+
+        public static float QDamage(Obj_AI_Base target)
+        {
+            return _Player.CalculateDamageOnUnit(target, DamageType.Magical,
+                (float) (new[] {80, 125, 170, 215, 260}[Program.Q.Level] + 0.8*_Player.FlatMagicDamageMod));
+        }
+    }
+}
