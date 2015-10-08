0c69de3453708a15afb347c7e16c2bf977cbd169
fueledbyflux <fueledbyflux@gmail.com>
2015-10-08 22:44:16 +1100

    moar modes.

--- OrbwalkerDisabler/OrbwalkerDisabler/Program.cs
+++ OrbwalkerDisabler/OrbwalkerDisabler/Program.cs
@@ -23,15 +23,61 @@ namespace OrbwalkerDisabler
         private static void Loading_OnLoadingComplete(EventArgs args)
         {
             Menu = MainMenu.AddMenu("Orbwalker Disabler", "orbws");
-            Menu.AddGroupLabel("Universal");
-            var a = Menu.Add("disableMovement", new CheckBox("Disable Movement", false));
-            var b = Menu.Add("disableAttacks", new CheckBox("Disable Attacks", false));
+            Menu.AddGroupLabel("Combo");
+            var ac = Menu.Add("disableMovementCombo", new CheckBox("Disable Movement", false));
+            var bc = Menu.Add("disableAttacksCombo", new CheckBox("Disable Attacks", false));
+
+            Menu.AddGroupLabel("Harass");
+            var ah = Menu.Add("disableMovementHarass", new CheckBox("Disable Movement", false));
+            var bh = Menu.Add("disableAttacksHarass", new CheckBox("Disable Attacks", false));
+
+            Menu.AddGroupLabel("WaveClear");
+            var awc = Menu.Add("disableMovementWaveClear", new CheckBox("Disable Movement", false));
+            var bwc = Menu.Add("disableAttacksWaveClear", new CheckBox("Disable Attacks", false));
+
+            Menu.AddGroupLabel("LastHit");
+            var alh = Menu.Add("disableMovementLastHit", new CheckBox("Disable Movement", false));
+            var blh = Menu.Add("disableAttacksLastHit", new CheckBox("Disable Attacks", false));
+
+            Menu.AddGroupLabel("Jungle");
+            var ajg = Menu.Add("disableMovementJungle", new CheckBox("Disable Movement", false));
+            var bjg = Menu.Add("disableAttacksJungle", new CheckBox("Disable Attacks", false));
+
+            Menu.AddGroupLabel("Flee");
+            var af = Menu.Add("disableMovementFlee", new CheckBox("Disable Movement", false));
 
 
             Game.OnTick += delegate
             {
-                Orbwalker.DisableAttacking = b.CurrentValue;
-                Orbwalker.DisableMovement = a.CurrentValue;
+                if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo))
+                {
+                    Orbwalker.DisableAttacking = bc.CurrentValue;
+                    Orbwalker.DisableMovement = ac.CurrentValue;
+                }
+                if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass))
+                {
+                    Orbwalker.DisableAttacking = bh.CurrentValue;
+                    Orbwalker.DisableMovement = ah.CurrentValue;
+                }
+                if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LaneClear))
+                {
+                    Orbwalker.DisableAttacking = bwc.CurrentValue;
+                    Orbwalker.DisableMovement = awc.CurrentValue;
+                }
+                if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LastHit))
+                {
+                    Orbwalker.DisableAttacking = blh.CurrentValue;
+                    Orbwalker.DisableMovement = alh.CurrentValue;
+                }
+                if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.JungleClear))
+                {
+                    Orbwalker.DisableAttacking = bjg.CurrentValue;
+                    Orbwalker.DisableMovement = ajg.CurrentValue;
+                }
+                if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Flee))
+                {
+                    Orbwalker.DisableMovement = af.CurrentValue;
+                }
             };
         }
     }
